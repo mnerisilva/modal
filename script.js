5994a9b91e6a45754a0322c45ed458891dab7ab9
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-07-23 12:57:00 -0300

    update

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ mask.addEventListener('click', function(e){
     }, 500);       
     mask.classList.remove('show');
 });
+
+
+
+/************* DEMONSTRAÇÃO DE COMO CRIAR UM ELEMENTO DINAMICAMENTE
+const body = document.querySelector('body');
+const elementoDiv = document.createElement('div');
+
+let texto = '<p><strong>JavaScript</strong> (frequentemente abreviado como JS) é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma (protótipos, orientado a objeto, imperativo e, funcional).[2][3] Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web. JavaScript permite páginas da Web interativas e, portanto, é uma parte essencial dos aplicativos da web. A grande maioria dos sites usa, e todos os principais navegadores têm um mecanismo JavaScript dedicado para executá-lo.</p>';
+body.appendChild(elementoDiv);
+elementoDiv.innerHTML = texto;
+
+elementoDiv.classList.add('modal-2');
+*/
